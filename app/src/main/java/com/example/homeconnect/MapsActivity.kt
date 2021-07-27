0d584d472eaecca26707b5d941b37dadcd5b4888
nclwam <nclnjoroge@gmail.com>
2021-07-27 14:25:28 +0300

    HomeConnect

---
+++ app/src/main/java/com/example/homeconnect/MapsActivity.kt
@@ -0,0 +1,69 @@
+package com.example.homeconnect
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.homeconnect.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.Marker
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+//    the default location
+    var MyPlace = LatLng(48.73, -73.99)
+
+
+//    a string location
+    var longitude= -73.99
+    var latitude = 48.73
+
+//    new pin
+    private lateinit var markerName: Marker
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+//        setonlick listerner
+        binding.mapbutton.setOnClickListener{
+            markerName.remove()
+
+            var location :String = binding.textmap.text.toString()//get user input
+            Toast.makeText(this,"You clicked:" +location, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(MyPlace).title("Here"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(MyPlace, 18f))
+    }
+}
